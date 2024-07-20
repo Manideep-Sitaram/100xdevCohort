ebdea09226d1d85f8d9d98c980bdb7584919e67c
Manideep-Sitaram <manideepsitaram2024phase1@gmail.com>
2024-07-20 21:27:43 +0530

    Grocery SHopping App

--- week-6.2/src/App.jsx
+++ week-6.2/src/App.jsx
@@ -3,30 +3,32 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
+  const [buttonId,setButtonId] = useState(1)
+return (
+  <>
+  <button onClick={() => setButtonId(1)}>1</button>
+  <button onClick={() => setButtonId(2)}>2</button>
+  <button onClick={() => setButtonId(3)}>3</button>
+  <button onClick={() => setButtonId(4)}>4</button>
+  <br />
+  <h3>Grocery Shopping</h3>
+    <Todo id={buttonId} />
+  </>
 
-  const [todos,setTodos] = useState([])
-  useEffect(function (){
-    
-    axios.get("https://sum-server.100xdevs.com/todos")
-    .then(res => {
-      console.log(res.data.todos)
-      setTodos(res.data.todos)
-  })
-
-  },[])
-
-  return (
-    <>
-    {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description}/>)}
-    </>
-  )
+)
 }
 
-function Todo ({title,description}){
+function Todo ({id}){
+  const [todo,setTodo] = useState({});
+  useEffect(function(){
+    axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
+    .then(res =>{console.log(res.data.todo); setTodo(res.data.todo)})
+  },[id])
+
   return (
     <>
-      <h3>{title}</h3>
-      <h5>{description}</h5>
+    <h3>{todo.title}</h3>
+    <h5>{todo.description}</h5>
     </>
   )
 }
