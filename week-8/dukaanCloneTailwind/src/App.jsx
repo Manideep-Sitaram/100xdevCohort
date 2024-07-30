df9b9eaad17e25f8940b481a3f506a893220d897
Manideep-Sitaram <manideepsitaram2024phase1@gmail.com>
2024-07-31 02:13:18 +0530

    more tyles added

--- week-8/dukaanCloneTailwind/src/App.jsx
+++ week-8/dukaanCloneTailwind/src/App.jsx
@@ -1,19 +1,36 @@
-import { useState } from 'react'
-import RevenueCard from './components/RevenueCard'
+import { useState } from "react";
+import RevenueCard from "./components/RevenueCard";
+import AppBar from "./components/AppBar";
 
 function App() {
- 
-
   return (
     <>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-
-      <RevenueCard title={"Next Payout"} amount={"₹2,312.23"} orderCount={23}/>
-      <RevenueCard title={"Amount Pending"} amount={"₹92,312.20"} orderCount={13}/>
-      <RevenueCard title={"Amount Processed"} amount={"₹23,92,312.19"} />
-    </div>
+      <div className="m-5">
+        <AppBar />
+        <div className="flex justify-between mb-5">
+          <div>Overview</div>
+          <div className="text-sm text-gray-600">
+            <select name="This Month" id="">
+              <option>This Month</option>
+            </select>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          <RevenueCard
+            title={"Next Payout"}
+            amount={"₹2,312.23"}
+            orderCount={23}
+          />
+          <RevenueCard
+            title={"Amount Pending"}
+            amount={"₹92,312.20"}
+            orderCount={13}
+          />
+          <RevenueCard title={"Amount Processed"} amount={"₹23,92,312.19"} />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
